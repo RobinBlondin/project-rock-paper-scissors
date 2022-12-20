3e33d02b536e39398d77d5679fe023791207be6d
Robin Blondin <robin.blondin@protonmail.com>
2022-12-20 20:49:21 +0100

    Added a loop and a playRound function.
    Added a while loop that keep asking the user for a correct input, if
    user input is wrong. Addded a playRound function which decides the winner
    between the user and computer.

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 
-let playerSelection = prompt('Please choose between rock, paper and scissors: ');
+let playerSelection = prompt('Please choose between rock, paper and scissors:'); // asking user for a selection
+
+while (["rock", "paper", "scissors"].indexOf(playerSelection.toLowerCase()) === -1) 
+{
+    playerSelection = prompt('You need to choose between rock, paper or scissors. Please try again!'); // Makes sure that user input is correct for this program to work
+    console.log(playerSelection);
+}
 
 let computerSelection = ()  => {
-    let rps = ['rock', 'paper', 'scissors'];
-    return rps[Math.floor(Math.random() * 3)]
+    let rps = ['rock', 'paper', 'scissors'];  
+    return rps[Math.floor(Math.random() * 3)];  // Stores a random choice into computerSelection
 };
 
 
-function rockPaperScissors(computerSelection, playerSelection) {
+function playRound(computerSelection, playerSelection) {
 
-
-
-
-
+    if (computerSelection == playerSelection.toLowerCase())
+    {
+        return "It's a draw!"
+    }
+    else if (computerSelection == 'rock')
+    {
+        return (playerSelection.toLowerCase() == 'paper') ? 'You win!' : 'You lose!'
+    }
+    else if (computerSelection == 'paper')
+    {
+        return (playerSelection.toLowerCase() == 'scissors') ? 'You win!' : 'You lose!'
+    }
+    else if (computerSelection == 'scissors')
+    {
+        return (playerSelection.toLowerCase() == 'rock') ? 'You win!' : 'You lose!'
+    }
 }
-console.log(computerSelection());
+console.log(playRound(computerSelection(), playerSelection));

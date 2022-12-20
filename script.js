d941409603e377148c0d00e8a9ba6a63d1eb0499
Robin Blondin <robin.blondin@protonmail.com>
2022-12-20 19:59:19 +0100

    Added a functions for computer selection.
    Added a function for the computer selection and a string variable that
    saves input from user using prompt.Added a functions for computer
    selection.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello');
+
+let playerSelection = prompt('Please choose between rock, paper and scissors: ');
+
+let computerSelection = ()  => {
+    let rps = ['rock', 'paper', 'scissors'];
+    return rps[Math.floor(Math.random() * 3)]
+};
+
+
+function rockPaperScissors(computerSelection, playerSelection) {
+
+
+
+
+
+}
+console.log(computerSelection());
